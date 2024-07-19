0af6ad13ad807e9f0850fc924ba452bd9333b05a
flamescripts <47398752+flamescripts@users.noreply.github.com>
2024-07-19 15:57:08 -0700

    Create url_dict.py

---
+++ url_dict.py
@@ -0,0 +1,221 @@
+fqdns = {
+	'Flow Production Tracking': [
+		"launchdarkly.shotgrid.autodesk.com"
+	],
+	'Flow Production Tracking Create': [
+		"sg-software.ems.autodesk.com",
+		"sg-sec.s3-accelerate.amazonaws.com"
+	],
+	'Flow Production Tracking RV': [
+		"sg-software.ems.autodesk.com"
+	],
+	'AWS S3': [
+		"sg-media-usor-01.s3.amazonaws.com",
+		"sg-media-tokyo.s3.amazonaws.com",
+		"sg-media-ireland.s3.amazonaws.com",
+		"sg-media-saopaulo.s3.amazonaws.com",
+		"sg-media-mumbai.s3.amazonaws.com",
+		"sg-media-sydney.s3.amazonaws.com"
+	],
+	'AWS S3 accelerated': [
+		"sg-media-usor-01.s3-accelerate.amazonaws.com",
+		"sg-media-tokyo.s3-accelerate.amazonaws.com",
+		"sg-media-ireland.s3-accelerate.amazonaws.com",
+		"sg-media-saopaulo.s3-accelerate.amazonaws.com",
+		"sg-media-mumbai.s3-accelerate.amazonaws.com",
+		"sg-media-sydney.s3-accelerate.amazonaws.com"
+	],
+	'Toolkit App Store': [
+		"tank.shotgunstudio.com",
+		"s3-proxy.shotgrid.autodesk.com",
+		"s3-proxy.shotgunstudio.com"
+	],
+	'Analytics': [
+		"api.amplitude.com"
+	]
+}
+
+subscription = {
+	'Login, License Entitlement': [
+		"www.autodesk.com",
+		"access.clic.autodesk.com",
+		"clm.clic.autodesk.com",
+		"accounts.autodesk.com",
+		"ipm-aem.autodesk.com",
+		"cdn.accounts.autodesk.com",
+		"cur.autodesk.com",
+		"registeronce.autodesk.com",
+		"developer.api.autodesk.com",
+		"auth.autodesk.com",
+		"native.auth.autodesk.com",
+		"profile.autodesk.com",
+		"api.userprofile.autodesk.com",
+		"cdn.web-platform.io",
+		"newassets.hcaptcha.com",
+		"js.hcaptcha.com",
+		"hcaptcha.com",
+		"imgs.hcaptcha.com",
+		"autologon.microsoftazuread-sso.com",
+		"autodesk-prod.okta.com",
+		"authn.autodesk.com"
+	],
+	'Application monitoring': [
+		"js-agent.newrelic.com",
+		"bam.nr-data.net"
+	],
+	'Microsoft SmartScreen Filter': [
+		"urs.microsoft.com"
+	],
+	'Web Analytics': [
+		"ssl.google-analytics.com",
+		"smetrics.autodesk.com",
+		"api.demandbase.com",
+		"munchkin.marketo.net",
+		"918-fod-433.mktoresp.com",
+		"dpm.demdex.net",
+		"autodesk.demdex.net",
+		"tags.tiqcdn.com",
+		"j.6sc.co",
+		"c.wa.autodesk.com",
+		"www.googletagmanager.com",
+		"www.google-analytics.com",
+		"m.eversttech.net",
+		"autodesk.tt.omtrdc.net"
+	],
+	'Third party Certificate download and verification sites': [
+		"s1.symcb.com",
+		"s2.symcb.com",
+		"sh.symcb.com",
+		"s.symcb.com",
+		"ss.symcb.com",
+		"ws.symantec.com",
+		"ocsp.ws.symantec.com",
+		"crl.ws.symantec.com",
+		"g.symcd.com",
+		"geotrust.com",
+		"crl.geotrust.com",
+		"pki.goog",
+		"ocsp.pki.goog",
+		"verisign.com",
+		"ss2.us",
+		"x.ss2.us",
+		"o.ss2.us",
+		"digicert.com",
+		"crl3.digicert.com",
+		"crl4.digicert.com",
+		"ocsp.digicert.com",
+		"amazontrust.com",
+		"crl.sca1b.amazontrust.com",
+		"crl.rootca1.amazontrust.com",
+		"ocsp.sca1b.amazontrust.com",
+		"ocsp.rootg2.amazontrust.com",
+		"crl.microsoft.com"
+	],
+	'Subscription Monitoring': [
+		"pubnubapi.com",
+		"autodesk.pubnubapi.com",
+		"api.pserver.sou.autodesk.com"
+	],
+	'Marketing': [
+		"ipm-aem.autodesk.com"
+	],
+	'Marketing (AdSSO)': [
+		"stats.g.doubleclick.net"
+	],
+	'In Product Purchase': [
+		"gateway.bluesnap.com",
+		"nexus.ensighten.com",
+		"d1rvg4h59z7jah.cloudfront.net"
+	],
+	'AutoCAD series products certificated hardware list downloading': [
+		"download.autodesk.com"
+	],
+	'ReCap sign in.': [
+		"akamaiedge.net"
+	],
+	'No longer used from AdSSO 11.0.0.1800': [
+		"s3.amazonaws.com"
+	]
+}
+
+identity = {
+	'Web Analytics': [
+		"tiqcdn.com",
+		"tags.tiqcdn.com",
+		"google-analytics.com",
+		"www.google-analytics.com",
+		"demandbase.com",
+		"api.demandbase.com",
+		"demandbase.com",
+		"api.demandbase.com",
+		"google.com",
+		"www.google.com",
+		"segment.io",
+		"api.segment.io",
+		"demdex.net",
+		"dpm.demdex.net",
+		"autodesk.demdex.net",
+		"everesttech.net",
+		"cm.everesttech.net",
+		"adnxs.com",
+		"secure.adnxs.com",
+		"amazonaws.com",
+		"s3.amazonaws.com",
+		"microsoftazuread-sso.com",
+		"autologon.microsoftazuread-sso.com",
+		"microsoftonline.com",
+		"login.microsoftonline.com",
+		"duosecurity.com",
+		"us.azureauth.duosecurity.com",
+		"api-0897e83e.duosecurity.com",
+		"oktacdn.com",
+		"live.com",
+		"login.live.com",
+		"msftauth.net",
+		"aadcdn.msftauth.net",
+		"msauthimages.net",
+		"aadcdn.msauthimages.net",
+		"hcaptcha.com",
+		"newassets.hcaptcha.com",
+		"hcaptcha.com",
+		"hcaptcha.com",
+		"imgs.hcaptcha.com",
+		"hcaptcha.com",
+		"okta.com",
+		"autodesk-prod.okta.com"
+	],
+	'Login': [
+		"autodesk.com",
+		"cdn.accounts.autodesk.com",
+		"accounts.autodesk.com",
+		"a.wa.autodesk.com",
+		"smetrics.autodesk.com",
+		"c.wa.autodesk.com",
+		"auth.autodesk.com",
+		"native.auth.autodesk.com",
+		"developer.api.autodesk.com",
+		"profile.autodesk.com",
+		"api.userprofile.autodesk.com",
+		"authn.autodesk.com",
+		"web-platform.io",
+		"cdn.web-platform.io"
+	],
+		'Application monitoring': [
+		"newrelic.com",
+		"js-agent.newrelic.com",
+		"nr-data.net",
+		"bam-cell.nr-data.net",
+		"6sc.co",
+		"j.6sc.co",
+		"c.6sc.co",
+		"6sense.com",
+		"epsilon.6sense.com",
+		"www.google-analytics.com",
+		"demandbase.com",
+		"api.demandbase.com"
+	],
+		'Marketing': [
+		"doubleclick.net",
+		"stats.g.doubleclick.net"
+	],
+}
